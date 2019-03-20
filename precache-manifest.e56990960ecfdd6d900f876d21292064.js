82607c6bd4aeba8eb105018a84b251543e58d31b
Marcin Kołodziej <marcinkolodziej15@gmail.com>
2019-03-20 18:50:29 +0100

    Updates

---
+++ precache-manifest.e56990960ecfdd6d900f876d21292064.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3cc08d862433c51764ec2475feeceb4",
+    "url": "/static/media/tags-illustration.b3cc08d8.svg"
+  },
+  {
+    "revision": "04e9160cfb96f85a92b6dd673449f86a",
+    "url": "/static/media/landing-header.04e9160c.jpg"
+  },
+  {
+    "revision": "354e72c1a1580209f3f97283285b9e5d",
+    "url": "/static/media/editor-illustration.354e72c1.svg"
+  },
+  {
+    "revision": "c071d2b77fdc241e7fc9be524bdeba42",
+    "url": "/static/media/access-illustration.c071d2b7.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "eead9ee599ef9e35d0d5",
+    "url": "/static/js/main.eead9ee5.chunk.js"
+  },
+  {
+    "revision": "9e73fd1235430dfb10c9",
+    "url": "/static/js/1.9e73fd12.chunk.js"
+  },
+  {
+    "revision": "5e93174331ed38d15c2046f751d1e498",
+    "url": "/index.html"
+  }
+];
